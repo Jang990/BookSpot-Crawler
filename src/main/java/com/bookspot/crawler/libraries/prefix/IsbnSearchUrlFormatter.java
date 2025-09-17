302308332ffe61484a605bf1e17660bdf9f307db
Jang <sdsd090811@gmail.com>
2025-09-17 17:27:21 +0900

    refactor: homePage -> dto로 변경

--- src/main/java/com/bookspot/crawler/libraries/prefix/IsbnSearchUrlFormatter.java
+++ src/main/java/com/bookspot/crawler/libraries/prefix/IsbnSearchUrlFormatter.java
@@ -3,7 +3,7 @@ package com.bookspot.crawler.libraries.prefix;
 import com.bookspot.crawler.libraries.file.LibraryPageDto;
 
 public interface IsbnSearchUrlFormatter {
-    boolean supports(LibraryPageDto homePage);
-    String getLibraryCode(LibraryPageDto homePage);
-    String format(LibraryPageDto homePage);
+    boolean supports(LibraryPageDto dto);
+    String getLibraryCode(LibraryPageDto dto);
+    String format(LibraryPageDto dto);
 }
