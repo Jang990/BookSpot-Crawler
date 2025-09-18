0e47d958b37335206cc8e83d76f8faf7ee2047c9
Jang <sdsd090811@gmail.com>
2025-09-18 10:23:49 +0900

    style: 설명 주석 추가

--- src/main/java/com/bookspot/crawler/libraries/prefix/UnsupportedSearchLibraryFormatter.java
+++ src/main/java/com/bookspot/crawler/libraries/prefix/UnsupportedSearchLibraryFormatter.java
@@ -2,6 +2,7 @@ package com.bookspot.crawler.libraries.prefix;
 
 import com.bookspot.crawler.libraries.file.LibraryPageDto;
 
+// 도서 검색 자체를 지원하지 않음.
 public abstract class UnsupportedSearchLibraryFormatter implements IsbnSearchUrlFormatter {
     @Override
     public abstract boolean supports(LibraryPageDto dto);
